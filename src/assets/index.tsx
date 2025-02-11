8a8734a1e7289376644fc044dac98e876e5f8f7b
RVPSA <akeshramasinghe@gmail.com>
2025-02-11 23:05:31 +0100

    Added whatsapp Icon

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -12,11 +12,12 @@ import leaf from "./leaf.jpg";
 import guestHouse2_1 from "./guestHouse2.1.png";
 import vision from "./vision.jpg";
 import QR from "./QR.png";
-import resort from './resort.jpg'
-import resortw from './resortwall.jpg';
-import resortAI from './resortAI.webp';
-import homeFull from './homeFull.png';
-import homeportrait from './homePortrait.png'
+import resort from "./resort.jpg";
+import resortw from "./resortwall.jpg";
+import resortAI from "./resortAI.webp";
+import whatsApp from "./whatsapp.svg";
+import homeFull from "./homeFull.png";
+import homeportrait from "./homePortrait.png";
 
 import { homePic } from "./home";
 
@@ -39,5 +40,6 @@ export {
   resortAI,
   resortw,
   homeFull,
-  homeportrait
+  homeportrait,
+  whatsApp,
 };
