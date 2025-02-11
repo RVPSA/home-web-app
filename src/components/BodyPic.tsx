3a4c6a7fd0a29a9eb5be0a7143d3c41749450a97
RVPSA <akeshramasinghe@gmail.com>
2025-02-11 20:27:30 +0100

    Develop hero picture area of the home page.

--- src/components/BodyPic.tsx
+++ src/components/BodyPic.tsx
@@ -1,17 +1,26 @@
-import { resort } from "../assets";
+import { resortAI } from "../assets";
 
 // <> Picture that is in the body of the home page.
 const BodyPic = () => {
   return (
     <>
       <div>
-        <div className="w-full h-[70vh] md:h-[60vh]">
-          <div className="w-full h-full object-fill">
+        <div className="w-full h-[70vh] md:h-[60vh] relative">
+          <div className="w-full h-full">
             <img
-              src={resort}
+              src={resortAI}
               alt="Image for body"
               className="w-full h-full object-cover"
             />
+          </div>
+          <div
+            className="absolute z-10 top-40 w-full flex flex-row justify-center md:text-xl md:tracking-[.50em] 
+          text-[#172554] font-semibold"
+          >
+            <h1 className="bg-[#93c5fd]/50 rounded p-1">
+              Your Home Away from Home - <br />
+              Where Comfort Meets Culture.
+            </h1>
           </div>
         </div>
       </div>
